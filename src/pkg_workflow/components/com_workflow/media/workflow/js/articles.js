9953793ccc30d90ef171b4fd3bd26c37cc421001
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-09-02 17:25:30 +0700

    Rewrite javascript function to support state refresh after transition done

--- src/pkg_workflow/components/com_workflow/media/workflow/js/articles.js
+++ src/pkg_workflow/components/com_workflow/media/workflow/js/articles.js
@@ -18,76 +18,91 @@ var WFArticles =
 	        var l = f.attr('action');
 		    var q = l.split('?');
 		    var u = q[0].replace('/administrator','');
-		    console.log('url to make a call for workflow is ' + u);
+		    //console.log('url to make a call for workflow is ' + u);
 	        jQuery("#articleList > tbody", f).find('tr').each( 
 	        	/* each row */	
 	        	function (rIdx, row) {
 	        		/* Find id of article */
 	        		var id = jQuery('td:nth-child(2)', this).find(':checkbox').prop('value');
 	        		var target = jQuery('td:nth-child(3)', this);
-	        		jQuery.ajax(
-	        		{
-	        			url: u + '?option=com_workflow',
-	        			data: 'task=instance.state&context=com_content.article&id=' + id + '&tmpl=component&format=json',
-	        			type: 'POST',
-	        			cache: false,
-	        			dataType: 'json',
-	        			success: function(resp)
-	        			{
-	        				if (resp.success == true) {
-	        					if (resp.data != 'undefined') {
-	        						var title = resp.data.title;
-	        						jQuery('ul.dropdown-menu', target).html('<li class="disabled"><a href="#"><span class="icon"> state: '+ title +'</span></a></li><li class="divider"></li>');
-	        					}
-	        				}
-	        			},
-	        			error: function(resp, e, msg)
-	        			{
-	        				//Workflow.displayMsg(resp, msg);
-	        			},
-	        			complete: function()
-	        			{
-	        				
-	        			}
-	        		});
-	        		/* Find state change button and replace dropdown items with JWorkflow 's */
+	        		/* Find state change buttons and disabled them */
 	        		
 	        		target.find('div > a')
 	        			.prop('onclick', null)
 	        			.addClass('disabled')
 	        			.prop('title', 'This behavior was disabled by JWorkflow');
 	        		
-	        		jQuery.ajax(
-	    	        {
-	    	        	url: u + '?option=com_workflow',
-	    	        	data: 'task=instance.transitions&context=com_content.article&id=' + id + '&tmpl=component&format=json',
-	    	        	type: 'POST',
-	    	        	cache: false,
-	    	        	dataType: 'json',
-	    	        	success: function(resp)
-	    	        	{
-	    	        		//console.log('query for transitions success for item id = '+id);
-	    	        		//console.log(resp.data[0].title);
-	    	        		if (resp.success == true) {
-	    	        			if (resp.data != 'undefined') {
-	    	        				var ix = 0;
-	    	        				for (ix = 0; ix < resp.data.length; ix++) {
-	    	        					jQuery('ul.dropdown-menu', target).append('<li><a href="#"><span class="icon"> '+ resp.data[ix].title +'</span></a></li>');
-	    	        				}	
-	    	        			}
-	    	        		}
-	    	        	},
-	    	        	error: function(resp, e, msg)
-	    	        	{
-	    	        		//Workflow.displayMsg(resp, msg);
-	    	        	},
-	    	        	complete: function()
-	    	        	{
-	    	        				
-	    	        	}
-	    	        });
-	        
+	        		WFArticles.loadWorkflowState(u, target, id);
 	        	}
 	        );
+		},
+		
+		loadWorkflowState : function(baseUrl, target, item_id) 
+		{
+
+    		jQuery('ul.dropdown-menu', target).html('<li class="divider"></li>');
+    		//console.log(jQuery('ul.dropdown-menu', target).html());
+    		jQuery.ajax(
+    		{
+    			url: baseUrl + '?option=com_workflow',
+    			data: 'task=instance.state&context=com_content.article&id=' + item_id + '&tmpl=component&format=json',
+    			type: 'POST',
+    			cache: false,
+    			dataType: 'json',
+    			success: function(resp)
+    			{
+    				if (resp.success == true) {
+    					if (resp.data != 'undefined') {
+    						var title = resp.data.title;
+    						jQuery('ul.dropdown-menu', target).prepend('<li class="disabled"><a href="#"><span class="icon"> state: '+ title +'</span></a></li>');
+    					}
+    				}
+    			},
+    			error: function(resp, e, msg)
+    			{
+    				//Workflow.displayMsg(resp, msg);
+    			},
+    			complete: function()
+    			{
+    				
+    			}
+    		});
+    		
+    		jQuery.ajax(
+	        {
+	        	url: baseUrl + '?option=com_workflow',
+	        	data: 'task=instance.transitions&context=com_content.article&id=' + item_id + '&tmpl=component&format=json',
+	        	type: 'POST',
+	        	cache: false,
+	        	dataType: 'json',
+	        	success: function(resp)
+	        	{
+	        		//console.log('query for transitions success for item id = '+id);
+	        		//console.log(resp.data[0].title);
+	        		if (resp.success == true) {
+	        			if (resp.data != 'undefined') {
+	        				var ix = 0;
+	        				for (ix = 0; ix < resp.data.length; ix++) {
+	        					//console.log(resp.data[ix].id);
+	        					jQuery('ul.dropdown-menu', target).append('<li><a href="#" class="wf-transition" item_id="'+item_id+'" transition="'+resp.data[ix].id+'"><span class="icon"> '+ resp.data[ix].title +'</span></a></li>');
+	        				}
+	        				
+	        				jQuery('a.wf-transition', target).click(function() {
+	        					var item_id = jQuery(this).attr('item_id');
+	        					var transition_id = jQuery(this).attr('transition');
+	        					alert('Transition ID = '+transition_id+', Item ID = '+item_id);
+	        				});
+	        			}
+	        		}
+	        	},
+	        	error: function(resp, e, msg)
+	        	{
+	        		//Workflow.displayMsg(resp, msg);
+	        	},
+	        	complete: function()
+	        	{
+	        				
+	        	}
+	        });			
 		}
 };
