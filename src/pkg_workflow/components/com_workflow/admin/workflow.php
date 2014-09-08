0b59bd107e7bb1e541d163eac24cf4c2d0eacc41
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2014-09-08 23:31:29 +0700

    Allow some task to be run even if not allow by ACL

--- src/pkg_workflow/components/com_workflow/admin/workflow.php
+++ src/pkg_workflow/components/com_workflow/admin/workflow.php
@@ -1,14 +1,21 @@
 <?php
 defined('_JEXEC') or die;
 
-// Access check.
-if (!JFactory::getUser()->authorise('core.manage', 'com_workflow')) {
-	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
-}
-
 // Include dependencies
 jimport('joomla.application.component.controller');
 jimport('workflow.framework');
+
+// Access check, need to allow some task for all users.
+$input = JFactory::getApplication()->input;
+$task = $input->get('task');
+
+$allowedTasks = array('instance.state', 'instance.validate', 
+		'instance.transition', 'instance.transitions');
+if (!in_array($task, $allowedTasks)) {
+	if (!JFactory::getUser()->authorise('core.manage', 'com_workflow')) {
+		return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+	}
+}
 
 //@todo Fix this CONSTANT not defined in library
 // define guard/action plugin path
