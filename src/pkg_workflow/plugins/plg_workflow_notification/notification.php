ea09f3ab99d2ce7304dd7069298ac827d9106f2a
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2015-05-09 21:42:20 +0700

    Missing plugin files

---
+++ src/pkg_workflow/plugins/plg_workflow_notification/notification.php
@@ -0,0 +1,54 @@
+<?php
+defined('_JEXEC') or die;
+jimport('joomla.plugin.plugin');
+
+/**
+ * Workflow Notification Plugin.
+ *
+ * @package     Workflow
+ * @subpackage  
+ * @since       1.0
+ */
+class plgWorkflowNotification extends JPlugin
+{
+	
+	protected $states = array('assigned', 'acked', 'resoloved');
+	/**
+	 * Notification related users on after transition occured
+	 *
+	 * @return  boolean  True if successful, false if not and a plugin error is set.
+	 * @since   1.0
+	 */
+	public function onWorkflowAfterTransition($context, $oInstance, $oUser, $comment, $oTransition, $oSourceState, $oTargetState)
+	{
+		// Adjust error condition as required.
+		$state = strtolower($oTargetState->title);
+		
+		if (!in_array($state, $this->states)) {
+		 	//do nothing
+	 		return true;
+	 	}
+
+	 	$receivers = $this->_getObservers($state, $oDocument);
+	 	
+		return true;
+	}
+	
+	/**
+	 * 
+	 * Get list of users to send e-mail to them
+	 */
+	protected function _getObservers($state, $item)
+	{
+		
+		if ($state == 'assigned') {
+			//	
+		}
+		else if ($state == 'acked') {
+			
+		}
+		else if ($state == 'resolved') {
+			
+		}	
+	}
+}
